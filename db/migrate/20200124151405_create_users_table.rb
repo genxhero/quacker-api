44ddbba513d82f5747f492a85779f9135c445326
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-24 07:30:04 -0800

    add more columns to user

--- db/migrate/20200124151405_create_users_table.rb
+++ db/migrate/20200124151405_create_users_table.rb
@@ -2,9 +2,15 @@ class CreateUsersTable < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :username, null: false
+      t.string :handle, null: false
+      #Degault handle is first name plus first two of last name plus random gobbledygook
+      t.string :first_name, null: false, default: "Anonymous"
+      t.string :last_name, null: false, default: ""
       t.string :email, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
+      t.integer :followings, array: true
+      t.integer :followers, array: true
       t.timestamps
     end
     add_index :users, :username
