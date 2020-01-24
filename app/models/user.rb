85957aa254b9843695704cbb8c33a5afb8868182
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-24 08:54:59 -0800

    work ruby magic to make sure every username has the samecase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   after_initialize :ensure_token
+  before_validation :format_vitals
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true, uniqueness: true
 
@@ -9,8 +10,19 @@ class User < ApplicationRecord
     self.save!
     self.session_token
   end
-  
+
   def ensure_token
     self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def format_vitals
+    self.email = self.email.downcase
+    self.first_name = self.first_name.downcase.capitalize
+    self.last_name = self.last_name.downcase.capitalize
+    if !self.username || self.username == ""
+        self.username = "#{self.first_name}#{self.last_name[0..1]}#{(0...8).map { ('0'..'9').to_a[rand(10)] }.join}"
+    else
+        self.username = self.username.downcase
+    end
   end
 end
---
+++ db/migrate/20200124165443_adddefaulttodob.rb
@@ -0,0 +1,4 @@
+class Adddefaulttodob < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
