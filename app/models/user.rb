d1f2a71170a2d163afefcc923b021ec02190b1ca
Aaron Goddard <genxhero@users.noreply.github.com>
2020-01-24 08:19:13 -0800

    successfully add date of birth column this time

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   after_initialize :ensure_token
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true, uniqueness: true
+
+  def reset_token
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+  
+  def ensure_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
 end
---
+++ db/migrate/20200124161523_addcolumntouserstaketwo.rb
@@ -0,0 +1,5 @@
+class Addcolumntouserstaketwo < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :dob, :date
+  end
+end
